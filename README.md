50e50afca310dfd49a7cf5c14f12c52b22bafaa9
Drew Skwiers-Koballa <drew.koballa@gmail.com>
2020-12-14 15:45:10 -0800

    catch for non-numeric querystring

---
+++ README.md
@@ -0,0 +1,8 @@
+# Temporarily Live for Testing in Azure
+
+## RecipeAPI
+https://kebab.azurewebsites.net/api/RecipeAPI?code=3LwapFdF8mCNGOg0yXAIhvpiyqnNCdIYPN8Au28fkHTzMMnLcJ8F1Q==&lamb=2
+
+## RecordAPI
+
+https://kebab.azurewebsites.net/api/RecordAPI?code=3LwapFdF8mCNGOg0yXAIhvpiyqnNCdIYPN8Au28fkHTzMMnLcJ8F1Q==
--- RecipeAPI/index.ts
+++ RecipeAPI/index.ts
@@ -3,31 +3,42 @@ import { KebabRecipe } from './recipe';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const lambKilos: number = +(req.query.lamb);
+    try {
+        const lambKilos: number = +(req.query.lamb);
+        if (!lambKilos) {
+            throw "Not a number";
+        }
 
-    let inputRecipe: KebabRecipe = new KebabRecipe(lambKilos);
+        let inputRecipe: KebabRecipe = new KebabRecipe(lambKilos);
 
-    let recipeResponse = {
-        "Ground Lamb": inputRecipe.lambKilos,
-        "Small Onions (minced)": inputRecipe.onions,
-        "Garlic Cloves (minced": inputRecipe.garlicCloves,
-        "Teaspoons Ground Cumin (divided)":  inputRecipe.cuminTeaspons,
-        "Teaspoons Ground Sumac (divided)": inputRecipe.sumacTeaspoons,
-        "Teaspoons Salt": inputRecipe.saltTeaspoons,
-        "Teaspoons Ground Black Pepper": inputRecipe.blackPepperTeaspoons,
-        "Teaspoons Red Pepper Flakes": inputRecipe.redPepperTeaspoons,
-        "Length (cm)": inputRecipe.lengthCm,
-        "Feeds": inputRecipe.feedsPeople
+        let recipeResponse = {
+            "Ground Lamb": inputRecipe.lambKilos,
+            "Small Onions (minced)": inputRecipe.onions,
+            "Garlic Cloves (minced": inputRecipe.garlicCloves,
+            "Teaspoons Ground Cumin (divided)":  inputRecipe.cuminTeaspons,
+            "Teaspoons Ground Sumac (divided)": inputRecipe.sumacTeaspoons,
+            "Teaspoons Salt": inputRecipe.saltTeaspoons,
+            "Teaspoons Ground Black Pepper": inputRecipe.blackPepperTeaspoons,
+            "Teaspoons Red Pepper Flakes": inputRecipe.redPepperTeaspoons,
+            "Length (cm)": inputRecipe.lengthCm,
+            "Feeds": inputRecipe.feedsPeople
+        }
+
+        let resp = {
+            status: 200,
+            body: recipeResponse
+        };
+        context.res = resp;
+
+        inputRecipe.RecordRecipe();
+        console.log("End of function");
+    } catch (err) {
+        let resp = {
+            status: 400,
+            body: { "Message": err }
+        };
+        context.res = resp;
     }
-
-    let resp = {
-        status: 200,
-        body: recipeResponse
-    };
-    context.res = resp;
-
-    inputRecipe.RecordRecipe();
-    console.log("End of function");
 };
 
 export default httpTrigger;
